b09302d767550f5b467458d69ca13bf14e4c7379
Sébastien BREDA <seb.gx@hotmail.fr>
2020-04-18 17:49:06 +0200

    Main menu proto

---
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public string s_game_scene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene(s_game_scene);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
